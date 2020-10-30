f8c618b4ecdd2fdb53227dd0a4b6c827d340280c
singizin <igsing96@gmail.com>
2020-10-30 20:31:24 +0700

    3

--- 3/4.4 E.py
+++ 3/4.4 E.py
@@ -2,8 +2,6 @@ N = int(input())
 road = [int(i) for i in input().split()]
 
 all_ground = sum(road)
-methods = 0
-h_set = set()
 ground_first = 0
 sep_pos = 0
 move_min = None
@@ -14,7 +12,6 @@ for i in range(1, N):
     h1 = ground_first / i
     h2 = ground_second / (N - i)
     move = 0
-
     for j in range(1, i):
         if road[j] > h1:
             move += road[j] - h1
@@ -23,6 +20,7 @@ for i in range(1, N):
             move += road[j] - h2
     if move_min == None:
         move_min = move
+        sep_pos = i
     if move < move_min:
         move_min = move
         sep_pos = i
