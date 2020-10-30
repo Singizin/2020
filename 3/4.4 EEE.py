9ee20c7f7fef47f4ecb6bc38abaf4b0e99399b8f
singizin <igsing96@gmail.com>
2020-10-30 21:20:18 +0700

    3

--- 3/4.4 EEE.py
+++ 3/4.4 EEE.py
@@ -14,7 +14,8 @@ def read_as_matrix():
 
 
 def read_as_dict():
-    N, interest = int(input())
+    temp = list(map(int, input().split()))
+    N, interest = temp[0], temp[1]
 
     islands = {str(x): [] for x in range(1, N + 1)}
 
@@ -22,25 +23,55 @@ def read_as_dict():
         a, b = input().split()
         islands[str(a)].extend([b])
         islands[str(b)].extend([a])
-    return islands, N
+    return islands, N, interest
 
 
-def paths(graph, start, end):
+def read_as_dict_set():
+    temp = list(map(int, input().split()))
+    N, interest = temp[0], temp[1]
+    islands = {str(x): set() for x in range(1, N + 1)}
+
+    for i in range(N - 1):
+        a, b = input().split()
+        islands[str(a)].add(b)
+        islands[str(b)].add(a)
+    return islands, N, interest
+
+
+def paths(graph, start, end, interest):
     todo = [[start, [start]]]
     while 0 < len(todo):
         (node, path) = todo.pop(0)
         for next_node in graph[node]:
+            # print(next_node)
             if next_node in path:
                 continue
             elif next_node == end:
                 yield path + [next_node]
             else:
                 todo.append([next_node, path + [next_node]])
+    print(path)
 
 
-graph_dict, N = read_as_dict()
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    # print('start-', start)
+    for next in graph.get(start) - visited:
+        dfs(graph, next, visited)
+    return visited
+
+
+# print(dfs(read_as_dict_set()[0], '1'))
+
+
+graph_dict, N, interest = read_as_dict()
 islands = list(graph_dict.keys())
 lenght = 0
+
+len_to_next = {x: 0 for x in islands}
+# print(len_to_next)
 for i in range(N):
     if i == N - 1:
         p = paths(graph_dict, islands[-1], islands[0])
@@ -48,9 +79,10 @@ for i in range(N):
         p = paths(graph_dict, islands[i], islands[i + 1])
     for i in p:
         lenght += len(i) - 1
-print(lenght)
+# print(lenght)
 
-'''
+
+"""
 8
 5 8
 1 3
@@ -59,4 +91,4 @@ print(lenght)
 2 8
 1 5
 4 5
-'''
+"""
