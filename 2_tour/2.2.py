03643d2b59dd50115c34d64fac17f58932eba433
singizin <igsing96@gmail.com>
2020-12-10 15:58:53 +0700

    123

--- 2_tour/2.2.py
+++ 2_tour/2.2.py
@@ -1,26 +1,32 @@
+import time
+
+
 def check_correct(line):
     cntrl_sum = 0
     for n in line:
         try:
             cntrl_sum += int(n)
         except:
-            continue
+            break
     if cntrl_sum != 16:
         return 1
     else:
         return 0
 
+
 broken = 0
 counter = 0
+actual_time = time.time()
+
 while True:
     try:
         line = input()
     except:
         break
-    if line == '':
+    if len(line) == 0:
         break
     broken += check_correct(line)
     counter += 1
     if counter > 0 and counter % 100 == 0:
-        print('t = 0.{}	N = {}'.format(counter//100, broken))
+        print('t = 0.{}\tN = {}'.format(counter // 100, broken))
         broken = 0
